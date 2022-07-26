bd08b3d35a9b21398c059dec096998375e59c743
Igor Bodrov <bodrovigor02@gmail.com>
2022-07-26 15:01:02 +0300

    feat: 🎸 Добавил стилизацию, исправил ошибки

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,15 @@ let headLinks = document.getElementById('head-links');
 
 burger.addEventListener('click', () => {
   navigation.classList.toggle('main-nav--opened');
+
   burger.classList.toggle('headline__burger--opened');
+
   headLinks.classList.toggle('headline__link-block--opened')
+})
+
+let uploadInput = document.getElementById('upload-input');
+let uploadButton = document.getElementById('upload-button');
+
+uploadButton.addEventListener('click', () => {
+  uploadInput.click();
 })
