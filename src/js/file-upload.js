badc836fd2b1c81bb71d92c310e32a4173e209a4
Igor Bodrov <bodrovigor02@gmail.com>
2022-07-31 11:47:43 +0300

    feat: 🎸 Добавил просмотр превью приложенных картинок

--- src/js/file-upload.js
+++ src/js/file-upload.js
@@ -1,6 +1,50 @@
-let appointmentFileInput = document.getElementById('appointment-file-input');
-let appointmentFileButton = document.getElementById('appointment-file-button');
+let attachInput = document.getElementById('attach-input');
+let attachButton = document.getElementById('attach-button');
+let attachList = document.getElementById('attach-list');
 
-appointmentFileButton.addEventListener('click', () => {
-  appointmentFileInput.click();
+let files = [];
+
+function addItem(event) {
+  if (!event.target.files.length) {
+    return;
+  }
+
+  files = Array.from(event.target.files);
+
+  files.forEach(file => {
+    const reader = new FileReader();
+
+    reader.onload = ev => {
+      const src = ev.target.result;
+      attachList.insertAdjacentHTML('beforeend', `
+
+        <li class="appointment__attach-item">
+          <button class="appointment__attach-cancel base-button" type="button" data-name="${file.name}"></button>
+          <img class="appointment__attach-img" src="${src}" alt="${file.name}">
+        </li>
+
+      `);
+    }
+
+    reader.readAsDataURL(file);
+  })
+}
+
+function removeItem(event) {
+  if (!event.target.dataset.name) {
+    return;
+  }
+  let name = event.target.dataset.name;
+  files = files.filter(file => file.name !== name);
+
+  let block = attachList.querySelector(`[data-name="${name}"]`).closest('.appointment__attach-item');
+  block.remove();
+}
+
+attachButton.addEventListener('click', () => {
+  attachInput.click();
 })
+
+attachInput.addEventListener('change', addItem);
+
+attachList.addEventListener('click', removeItem);
