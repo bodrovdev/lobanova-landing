a3b0b2d7ad10c16c3d06a23a460fa3e53419063a
Igor Bodrov <bodrovigor02@gmail.com>
2022-08-08 08:54:17 +0300

    feat: 🎸 Добавил стилизацию

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,25 @@
+import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
+
 let burger = document.getElementById('burger');
 let navigation = document.getElementById('main-nav');
-let navigationLinks = document.querySelectorAll('.main-nav__list-link');
+let navigationLinks = document.querySelectorAll('.main-nav__link');
 
 burger.addEventListener('click', () => {
   navigation.classList.toggle('main-nav--opened');
   burger.classList.toggle('headline__burger--opened');
 });
 
+burger.addEventListener('click', () => {
+  if (burger.classList.contains('headline__burger--opened')) {
+    disableBodyScroll(navigation);
+  }
+  enableBodyScroll(navigation);
+})
+
 navigationLinks.forEach((element) => {
   element.addEventListener('click', () => {
     navigation.classList.remove('main-nav--opened');
     burger.classList.remove('headline__burger--opened');
+    enableBodyScroll(navigation);
   });
 });
